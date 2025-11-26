123ce1b3bf91f80999173504ef30c1719bed97a9
kevin <1622929837@qq.com>
2025-11-26 16:23:23 +0800

    build(project): create a new koishi plugin

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema } from "koishi";
 
-export const name = 'dice-dagger-heart'
+export const name = "dice-dagger-heart";
 
 export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
   // write your plugin here
+  ctx.on("message", (session) => {
+    if (session.content === "!dice") {
+      session.send("你掷出了一个骰子");
+    }
+  });
 }
